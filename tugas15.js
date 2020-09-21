e1be53708624c9cd5d72115a970dda6eaf1798e5
hidayatul-rk <71365495+hidayatul-rk@users.noreply.github.com>
2020-09-21 10:18:53 +0700

    Add files via upload

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+var ab = 8
+var bc = 6
+var hasil;
+
+n = (Math.pow(8,2)) + (Math.pow(6,2))
+hasil = Math.sqrt(n)
+
+	console.log ("Nilai AB = 8")
+	console.log ("Nilai BC = 6")
+
+	console.log ("Panjang sisi aC pada segitiga siku2 tersebut adalah :", hasil, "cm")
